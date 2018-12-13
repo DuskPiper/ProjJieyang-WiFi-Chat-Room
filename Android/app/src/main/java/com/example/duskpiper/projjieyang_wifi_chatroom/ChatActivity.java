85f1c721c393a35f20dbdc0ab983d15ad6fb2d3d
R Zhang <detchue@gmail.com>
2018-12-13 16:51:11 -0500

    send location into chatroom

--- Android/app/src/main/java/com/example/duskpiper/projjieyang_wifi_chatroom/ChatActivity.java
+++ Android/app/src/main/java/com/example/duskpiper/projjieyang_wifi_chatroom/ChatActivity.java
@@ -1,11 +1,18 @@
 package com.example.duskpiper.projjieyang_wifi_chatroom;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.StrictMode;
 import android.os.Vibrator;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,12 +23,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity implements SocketThread.OnClientListener {
     private EditText inputMessage;
     private EditText inputIP;
     private EditText inputPort;
     private Button sendButton;
+    private Button sendLocationButton;
     private Button setAddrButton;
     private Button cancelConnectionButton;
     private TextView debugWindow;
@@ -36,6 +45,10 @@ public class ChatActivity extends AppCompatActivity implements SocketThread.OnCl
     private boolean looperDaemon = true;
     private SocketThread socketThread;
     private String myName;
+    private LocationManager locationManager;
+    private String locationProvider;
+
+    public int MY_PERMISSION_REQUEST_FINE_LOCATION = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,12 +61,15 @@ public class ChatActivity extends AppCompatActivity implements SocketThread.OnCl
 
         inputMessage = (EditText)findViewById(R.id.message_input);
         sendButton = (Button)findViewById(R.id.send_button);
+        sendLocationButton = (Button)findViewById(R.id.send_location_button);
         debugWindow = (TextView)findViewById(R.id.debug_window);
         inputIP = (EditText)findViewById(R.id.ip_edit);
         inputPort = (EditText)findViewById(R.id.port_edit);
         setAddrButton = (Button)findViewById(R.id.set_addr_button);
         cancelConnectionButton = (Button)findViewById(R.id.disconnect_button);
         chatWindow = (TextView)findViewById(R.id.chat_window);
+
+        sendLocationButton.setVisibility(View.INVISIBLE);
 
         host = "172.31.129.73";
         port = 999;
@@ -83,8 +99,52 @@ public class ChatActivity extends AppCompatActivity implements SocketThread.OnCl
                     socketThread.sendMessage(message);
                     if (myName == null) {
                         myName = message;
+                        sendLocationButton.setVisibility(View.VISIBLE);
                     }
                 }
+            }
+        });
+
+        sendLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // FIND AND SEND LOCATION
+                // Check and grant permission
+                if (ContextCompat.checkSelfPermission(ChatActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ChatActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            MY_PERMISSION_REQUEST_FINE_LOCATION);
+                    Log.e("Location" , "Permission denied by system");
+                } else {
+                    // Permission granted
+                    locationManager = (LocationManager)ChatActivity.this.getSystemService(Context.LOCATION_SERVICE);
+                    List<String> providers = locationManager.getProviders(true);
+                    if (providers.contains(LocationManager.GPS_PROVIDER)) {
+                        locationProvider = LocationManager.GPS_PROVIDER;
+                        Log.d("Location", "Using GPS");
+                    } else if (providers.contains(LocationManager.NETWORK_PROVIDER)) {
+                        locationProvider = LocationManager.NETWORK_PROVIDER;
+                        Log.d("Location", "Using Network");
+                    } else {
+                        Log.e("Location", "No provider");
+                        Toast.makeText(ChatActivity.this, "No Location Provider Found", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    // Acquire Location
+                    Location location = locationManager.getLastKnownLocation(locationProvider);
+                    if (location != null) {
+                        String lat = Double.toString(location.getLatitude());
+                        String lng = Double.toString(location.getLongitude());
+                        if (lat != null && lng != null) {
+                            socketThread.sendMessage("I'm at [" + lat + ", " + lng + "]");
+                        }
+                    } else {
+                        Log.e("Location", "Location is NULL");
+                    }
+                }
+
             }
         });
 
